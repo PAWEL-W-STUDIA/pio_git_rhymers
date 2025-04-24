66180ae2536f028f463acf3225d62d4aa89997e7
PAWEL-W-STUDIA <158048182+PAWEL-W-STUDIA@users.noreply.github.com>
2025-04-24 17:13:32 +0200

    Changed FIFORhymer to FifoRhymer

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -1,11 +1,11 @@
 package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.FifoRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-public class DefaultRhymersFactory implements Rhymersfactory {
+public class DefaultRhymersFactory implements RhymersFactory {
 
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
@@ -18,8 +18,8 @@ public class DefaultRhymersFactory implements Rhymersfactory {
     }
 
     @Override
-    public DefaultCountingOutRhymer getFIFORhymer() {
-        return new FIFORhymer();
+    public DefaultCountingOutRhymer getFifoRhymer() {
+        return new FifoRhymer();
     }
 
     @Override
