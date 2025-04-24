ce6b8ee663784262f2457b2ca4eb0f42e0e797e2
PAWEL-W-STUDIA <158048182+PAWEL-W-STUDIA@users.noreply.github.com>
2025-04-24 17:16:16 +0200

    Changed names of interface and method

--- src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
-public interface Rhymersfactory {
+public interface RhymersFactory {
 
     public DefaultCountingOutRhymer getStandardRhymer();
 
     public DefaultCountingOutRhymer getFalseRhymer();
 
-    public DefaultCountingOutRhymer getFIFORhymer();
+    public DefaultCountingOutRhymer getFifoRhymer();
 
     public DefaultCountingOutRhymer getHanoiRhymer();
 
